954e44612cabf6177232adcc095068997b3d81b1
Adrian Hesketh <adrianhesketh@hushmail.com>
2020-12-21 21:07:11 +0000

    Split the operating system level and user level configuration.

---
+++ nixos/home/adrian/home-manager-configure.sh
@@ -0,0 +1,4 @@
+# Add home manager.
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
--- nixos/install.sh
+++ nixos/install.sh
@@ -5,8 +5,3 @@ cp -r ./etc /etc
 
 # Rebuild NixOS.
 nixos-rebuild switch
-
-# Add home manager.
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
