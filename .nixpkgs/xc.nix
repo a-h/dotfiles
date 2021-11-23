7a121a9291d875146f520d538891542e0c63c988
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-11-23 15:26:30 +0000

    udpate xc

--- .nixpkgs/xc.nix
+++ .nixpkgs/xc.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "xc";
-  version = "0.44";
+  version = "0.45";
 
   src = fetchFromGitHub {
     owner = "Joe-Davidson1802";
     repo = "xc";
-    rev = "0902dadea5f556cc578a7254ee45603da9514fb9";
-    sha256 = "0wbfbj5bqiq5zzw9x7mhlsqn6lfn1gbl7m6f5vg7wn7hmj6qksqy";
+    rev = "2d8273a010d655576034e06e38a0c900c3b0638c";
+    sha256 = "0k5q2f1h1w0xy2301fn450bm82kaxyv6v288sym2dzsm9s03smsa";
   };
 
   vendorSha256 = "1ryb8m407rijrwlxcpkwyayjf1raxi2qw4ndpjsi12q1bjhx0y33";
