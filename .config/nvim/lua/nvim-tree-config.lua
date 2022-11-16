08e97e1776af85a4dbc733a4ee34acf36114c6fb
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-11-16 14:34:38 +0000

    feat: quit nvim-tree on open

--- .config/nvim/lua/nvim-tree-config.lua
+++ .config/nvim/lua/nvim-tree-config.lua
@@ -1,4 +1,10 @@
 require("nvim-web-devicons").setup()
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+ actions = {
+   open_file = {
+     quit_on_open = true
+   }
+ }
+})
 
 vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
