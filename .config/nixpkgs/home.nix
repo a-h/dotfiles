75b742ba41f3ba0f5dddd049a6b5ff70f641e3ce
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-01-02 15:40:36 +0000

    use the right font

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -133,6 +133,8 @@ in
     enableScDaemon = true;
   };
 
+  fonts.fontconfig.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
