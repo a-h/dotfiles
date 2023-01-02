2e2d999983a998358bf4628db7d5e24659469b14
Edward Hesketh <headblockhead@gmail.com>
2023-01-02 18:43:17 +0000

    fixed issues with nvim lua config (#1)
    * fixed issues with nvim lua config
    
    * updated nvim plugins
    
    The pinned revision of tresitter was replaced by a nixpkgs definition
    equivalent to it.
    
    * update the fonts
    
    Co-authored-by: Adrian Hesketh <adrianhesketh@hushmail.com>

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,8 +1,26 @@
 -- Set colors.
 -- To find available colours to set, use `:hi <name>` to search through
 -- available colours.
-vim.cmd[[colorscheme dracula]]
-vim.cmd[[set background=dark]]
-vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
+local dracula = require("dracula")
+dracula.setup({
+  -- set custom lualine background color
+  lualine_bg_color = "#44475a", -- default nil
+  -- set italic comment
+  italic_comment = true, -- default false
+  transparent_bg = true,
+  -- overrides the default highlights see `:h synIDattr`
+  overrides = {
+    -- Examples
+    -- NonText = { fg = dracula.colors().white }, -- set NonText fg to white
+    -- NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+    -- Nothing = {} -- clear highlight of Nothing
+  },
+})
+vim.cmd [[colorscheme dracula]]
+require('lualine').setup {
+  options = {
+    theme = 'dracula-nvim'
+  }
+}
 -- Floating window.
-vim.cmd[[hi Pmenu guibg=NONE]]
+vim.cmd [[hi Pmenu guibg=NONE]]
