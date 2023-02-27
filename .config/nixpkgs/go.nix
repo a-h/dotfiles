ae7d2f6594b7dac90e2b4f41835ad1042852d75d
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-02-27 08:14:12 +0000

    update go to v1.20.1

--- .config/nixpkgs/go.nix
+++ .config/nixpkgs/go.nix
@@ -7,15 +7,15 @@ let
   hashes = {
     # Use `print-hashes.sh ${version}` to generate the list below
     # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/compilers/go/print-hashes.sh
-    darwin-amd64 = "16f8047d7b627699b3773680098fbaf7cc962b7db02b3e02726f78c4db26dfde";
-    darwin-arm64 = "35d819df25197c0be45f36ce849b994bba3b0559b76d4538b910d28f6395c00d";
-    linux-386 = "ba8c97965e0856c69c9ca2c86f96bec5bb21de43e6533e25494bb211d85cda1b";
-    linux-amd64 = "5e8c5a74fe6470dd7e055a461acda8bb4050ead8c2df70f227e3ff7d8eb7eeb6";
-    linux-arm64 = "b62a8d9654436c67c14a0c91e931d50440541f09eb991a987536cb982903126d";
-    linux-armv6l = "f3ccec7816ecd704ebafd130b08b8ad97c55402a8193a107b63e9de12ab90118";
-    linux-ppc64le = "37e1d4342f7103aeb9babeabe8c71ef3dba23db28db525071119e94b2aa21d7d";
-    linux-s390x = "51b45dec41295215df17f78e67d1a373b9dda97a5e539bed440974da5ffc97de";
-  };
+    darwin-amd64 = "a300a45e801ab459f3008aae5bb9efbe9a6de9bcd12388f5ca9bbd14f70236de";
+    darwin-arm64 = "f1a8e06c7f1ba1c008313577f3f58132eb166a41ceb95ce6e9af30bc5a3efca4";
+    linux-386 = "3a7345036ebd92455b653e4b4f6aaf4f7e1f91f4ced33b23d7059159cec5f4d7";
+    linux-amd64 = "000a5b1fca4f75895f78befeb2eecf10bfff3c428597f3f1e69133b63b911b02";
+    linux-arm64 = "5e5e2926733595e6f3c5b5ad1089afac11c1490351855e87849d0e7702b1ec2e";
+    linux-armv6l = "e4edc05558ab3657ba3dddb909209463cee38df9c1996893dd08cde274915003";
+    linux-ppc64le = "85cfd4b89b48c94030783b6e9e619e35557862358b846064636361421d0b0c52";
+    linux-s390x = "ba3a14381ed4538216dec3ea72b35731750597edd851cece1eb120edf7d60149";
+};
 
   toGoCPU = platform: {
     "i686" = "386";
@@ -26,7 +26,7 @@ let
     "powerpc64le" = "ppc64le";
   }.${platform.parsed.cpu.name} or (throw "Unsupported CPU ${platform.parsed.cpu.name}");
 
-  version = "1.19.2";
+  version = "1.20.1";
 
   toGoPlatform = platform: "${toGoKernel platform}-${toGoCPU platform}";
 
