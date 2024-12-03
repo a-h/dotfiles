1561c58b6ce8a7ff8b1bcd2129e699f8d646ec10
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-12-03 09:11:17 +0000

    feat: add copilot chat

--- .config/nvim/lua/fzf.lua
+++ .config/nvim/lua/fzf.lua
@@ -1,10 +1,9 @@
 -- FZF to replace ctrlP
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
+  local options = { noremap = true }
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-map("n", "<C-p>", "<cmd>GFiles<cr>", {silent = true, noremap = true})
-map("n", "<space>p", "<cmd>Ag<cr>", {silent = true, noremap = true})
-map("n", "<space>a", "<cmd>Files<cr>", {silent = true, noremap = true})
-
+map("n", "<C-p>", "<cmd>GFiles<cr>", { silent = true, noremap = true })
+map("n", "<space>p", "<cmd>Ag<cr>", { silent = true, noremap = true })
+map("n", "<space>o", "<cmd>Files<cr>", { silent = true, noremap = true })
