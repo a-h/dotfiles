2c6eeedc3dfa90b1f6f7f5de9ad2820b3fd25892
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-12-30 14:03:05 +0000

    chore: neovim moans about UTF-8 instead of utf-8 now

--- .config/nvim/lua/goimports.lua
+++ .config/nvim/lua/goimports.lua
@@ -3,12 +3,12 @@
 
 function OrgImports(wait_ms)
   local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
+  params.context = { only = { "source.organizeImports" } }
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
   for _, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
       else
         vim.lsp.buf.execute_command(r.command)
       end
