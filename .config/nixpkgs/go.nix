388e605bee7695a9672744168a2e0f6b1935f573
Adrian Hesketh <adrianhesketh@hushmail.com>
2025-03-31 09:42:17 +0100

    chore: bump to go 1.24.1

--- .config/nixpkgs/go.nix
+++ .config/nixpkgs/go.nix
@@ -7,10 +7,10 @@ let
   hashes = {
     # Use `print-hashes.sh ${version}` to generate the list below
     # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/compilers/go/print-hashes.sh
-    darwin-amd64 = "7af054e5088b68c24b3d6e135e5ca8d91bbd5a05cb7f7f0187367b3e6e9e05ee";
-    darwin-arm64 = "fd9cfb5dd6c75a347cfc641a253f0db1cebaca16b0dd37965351c6184ba595e4";
-    linux-amd64 = "dea9ca38a0b852a74e81c26134671af7c0fbe65d81b0dc1c5bfe22cf7d4c8858";
-    linux-arm64 = "c3fa6d16ffa261091a5617145553c71d21435ce547e44cc6dfb7470865527cc7";
+    darwin-amd64 = "addbfce2056744962e2d7436313ab93486660cf7a2e066d171b9d6f2da7c7abe";
+    darwin-arm64 = "295581b5619acc92f5106e5bcb05c51869337eb19742fdfa6c8346c18e78ff88";
+    linux-amd64 = "cb2396bae64183cdccf81a9a6df0aea3bce9511fc21469fb89a0c00470088073";
+    linux-arm64 = "8df5750ffc0281017fb6070fba450f5d22b600a02081dceef47966ffaf36a3af";
   };
 
   toGoCPU = platform: {
@@ -22,7 +22,7 @@ let
     "powerpc64le" = "ppc64le";
   }.${platform.parsed.cpu.name} or (throw "Unsupported CPU ${platform.parsed.cpu.name}");
 
-  version = "1.24.0";
+  version = "1.24.1";
 
   toGoPlatform = platform: "${toGoKernel platform}-${toGoCPU platform}";
 
