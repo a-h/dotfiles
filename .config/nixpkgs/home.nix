bb8a961b6b0f19d7c4fc780b85d32f648c1d8879
Adrian Hesketh <inbox@adrianhesketh.com>
2026-07-02 10:24:03 +0100

    chore: minor update

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -55,7 +55,10 @@ in
     };
     gtk2.extraConfig = "gtk-application-prefer-dark-theme = \"true\"";
     gtk3.extraConfig = { "gtk-application-prefer-dark-theme" = "true"; };
-    gtk4.extraConfig = { "gtk-application-prefer-dark-theme" = "true"; };
+    gtk4 = {
+      theme = config.gtk.theme;
+      extraConfig = { "gtk-application-prefer-dark-theme" = "true"; };
+    };
   };
 
   dconf = {
